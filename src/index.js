bc310029c2e70f067eb22aa27bbf344703973aa5
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-11-25 15:52:23 -0300

    config dotenv end express

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ const express = require('express')
 const https = require('https')
 const fs = require('fs')
 const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const privateKey  = fs.readFileSync('./.cert/key.pem', 'utf8');
 const certificate = fs.readFileSync('./.cert/cert.pem', 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 
 const httpsServer = https.createServer(credentials, app);
+
+app.use(cors)
+app.use(express.json)
+
+
 
 httpsServer.listen(4000,()=>{
   console.log('serve is running')
